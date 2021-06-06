ac88786b8a1dc4ba06d59475e617f92ea6b3d50c
Raphael HEBERT <raph.heb@gmail.com>
2021-06-06 21:36:08 +0200

    Update post install script to incorporate karabiner special setup

--- postbrew.sh
+++ postbrew.sh
@@ -2,14 +2,26 @@
 set -e
 
 cd $HOME/dotfiles
-Repo=("kitty karabiner skhd yabai nvim zsh ")
+Repo=("kitty skhd yabai nvim zsh ")
 for repo in $Repo
 do
   mkdir $HOME/.config/$repo
   stow -vt ~/.config/$repo $repo
 done
 
+rm -Rf ~/.config/karabiner
+stow -vt ~/.config karabiner 
 
+# Making symbolic link example
+# The following command allows you to put karabiner.json on ~/Dropbox/private.
+
+#     mv ~/.config/karabiner ~/Dropbox/private
+#     ln -s ~/Dropbox/private/karabiner ~/.config
+
+
+# You have to restart karabiner_console_user_server process by the following command after you made a symlink in ordre to tell Karabiner-Elements that the parent directory is changed.
+
+#     launchctl kickstart -k gui/`id -u`/org.pqrs.karabiner.karabiner_console_user_server
 
 
 
