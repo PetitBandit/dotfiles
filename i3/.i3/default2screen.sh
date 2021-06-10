a7cd6a872f7872776e82f5cdce5f93e96cb0e5de
Raphael HEBERT <raph.heb@gmail.com>
2021-06-10 09:43:46 +0200

    adding i3 to the dotfiles

---
+++ i3/.i3/default2screen.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-0 --off --output DisplayPort-1 --off --output HDMI-A-0 --mode 1920x1080 --pos 3840x322 --rotate normal --output HDMI-A-1 --mode 3840x2160 --pos 0x0 --rotate normal --output DVI-D-0 --off
